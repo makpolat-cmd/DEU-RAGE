2b4550bdd183deb1fbe0d37c26b4dcd8c6ce2949
Kürşat Görmez <kursatgormez@gmail.com>
2020-03-01 23:19:39 +0300

    deneme

--- motorkontrol.py
+++ motorkontrol.py
@@ -1,15 +1,18 @@
 import RPi.GPIO as IO
 import time
 
+
+
+m1 = IO.setup(12, IO.OUT,5,6)
+m2 = IO.setup(13, IO.OUT,5,6)
+    
 def motorsInit():
     IO.setmode(IO.BCM)
-    m1 = IO.setup(12, IO.OUT)
-    m2 = IO.setup(13, IO.OUT)
     time.sleep(1)
-
+    
 def motorStart(dutycyle=1000):
-    m1.start(dutycycle)
-    m2.start(dutycycle)
+    m1.start(dutycyle)
+    m2.start(dutycyle)
     time.sleep(1)
    
 def motorsStop():
