b7d9cd5f8f043a360ddb0800ed2e7283dbad587d
Kürşat Görmez <kursatgormez@gmail.com>
2020-03-01 22:23:27 +0300

    sleep eklendi

--- motorkontrol.py
+++ motorkontrol.py
@@ -7,17 +7,22 @@ def motorsInit():
     IO.setmode(IO.BCM)
     m1 = IO.setup(12, IO.OUT)
     m2 = IO.setup(13, IO.OUT)
+    time.sleep(1)
 
 def motorStart(dutycyle=1000):
     m1.start(dutycycle)
     m2.start(dutycycle)
+    time.sleep(1)
    
 def motorsStop():
     m1.ChangeDutyCycle(0)
     m2.ChangeDutyCycle(0)
+    time.sleep(1)
 
 def motor1speed(speed = 0):
     m1.ChangeDutyCycle(speed)
+    time.sleep(1)
 
 def motor2speed(speed = 0):
     m2.ChangeDutyCycle(speed)
+    time.sleep(1)
