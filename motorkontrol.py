da0a2abb709d791fc464c4ed31301d22109f35b8
Kürşat Görmez <kursatgormez@gmail.com>
2020-03-01 22:12:56 +0300

    deneme

---
+++ motorkontrol.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as IO
+from time
+
+
+def motorsInit()
+    
+    IO.setmode(IO.BCM)
+    m1 = IO.setup(12, IO.OUT)
+    m2 = IO.setup(13, IO.OUT)
+
+def motorStart(dutycyle=1000)
+    m1.start(dutycycle)
+    m2.start(dutycycle)
+   
+def motorStop()
+    m1.ChangeDutyCycle(0)
+    m2.ChangeDutyCycle(0)
+
+def motor1speed(speed = 0)
+    m1.ChangeDutyCycle(speed)
+
+def motor2speed(speed = 0)
+    m2.ChangeDutyCycle(speed)
--- slm.py
+++
@@ -1,9 +0,0 @@
-
-print("slm")
-print("as")
-print("mrb enes")
-
-for a in range(9) :
-    print(a)
-    print("\n hahaha ---")
-print("cihat napan")
