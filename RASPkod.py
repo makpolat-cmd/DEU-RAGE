65accd8d1f10388d82a1ea1ba7ab4fa96b08b82a
enes <ayduran2021@gmail.com>
2020-04-17 22:31:03 +0300

    Create RASPkod.py

---
+++ RASPkod.py
@@ -0,0 +1,48 @@
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+def roi(image):
+    height = image.shape[0]
+    polygons = np.array([
+    [(150, height ),(550, height), (300,250)]
+    ])
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, polygons, 255)
+    masked_image = cv2.bitwise_and(image, mask)
+    return masked_image
+
+
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+camera.vflip = True
+# allow the camera to warmup
+time.sleep(0.1)
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    # grab the raw NumPy array representing the image, then initialize the timestamp
+    # and occupied/unoccupied text
+    image = frame.array
+    lane_image = np.copy(image)
+    gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    canny = cv2.Canny(blur, 50, 150)
+    
+    cropped_image = roi(canny)
+    
+    
+    # show the frame
+    cv2.imshow("Frame", cropped_image)
+    key = cv2.waitKey(1) & 0xFF
+    # clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
